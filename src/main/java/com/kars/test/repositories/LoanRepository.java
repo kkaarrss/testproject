d14b855afe4109a38848f47258496409db627a56
Kars Plus <kars.plus@rabobank.nl>
2022-09-28 13:52:22 +0200

    first

--- src/main/java/com/kars/test/repositories/LoanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kars.test.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.kars.test.model.Loan;
-
-@Repository
-public interface LoanRepository extends JpaRepository<Loan, Long> {}
