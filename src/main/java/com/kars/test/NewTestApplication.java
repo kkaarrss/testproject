3434a9aa66d8ee81f31439056a31e91bb267a07a
Kars Plus <kars.plus@rabobank.nl>
2022-09-28 13:01:56 +0200

    first

--- src/main/java/com/kars/test/NewTestApplication.java
+++ src/main/java/com/kars/test/NewTestApplication.java
@@ -3,7 +3,10 @@ package com.kars.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class NewTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewTestApplication.class, args);
